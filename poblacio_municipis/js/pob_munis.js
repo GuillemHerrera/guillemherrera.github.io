617bf0427a4fc11ec59a1ca0a2a23195d8adb651
guillemherrera <guillem.herrera@gmail.com>
2019-04-10 23:48:02 +0200

    Updating Portfolio

--- poblacio_municipis/js/pob_munis.js
+++ poblacio_municipis/js/pob_munis.js
@@ -2,7 +2,7 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapboxgl-styles/mapa_municipal.json',
     hash: true,
-    center: [-1.532, 41.8],
+    center: [-1.517, 41.8],
     zoom: 7
 });
 
